084ac21dd1ebd783f78a0286e66d87d2b326c951
MMAzeem3 <musasa12399@gmail.com>
2022-03-24 18:32:25 -0400

    huh

--- inc/shared-memory-struct.h
+++ inc/shared-memory-struct.h
@@ -4,7 +4,6 @@
 #include <string>
 
 struct SmStruct{
-<<<<<<< HEAD
     const size_t file_path_length = 512;
     char file_path[512];
     const size_t kSearch_len = 512;
@@ -13,14 +12,6 @@ struct SmStruct{
     char path[4096-sizeof(int)-512-sizeof(int)-512];      //extended by external code
     // std::vector<std::string> file_lines;
     // char file_lines[2000];
-=======
-    const size_t path_length = 512;
-    char path[512];
-    // const size_t kSearch_len = 512;
-    // char search_str[512];
-    const size_t bufferSize = 4096 - sizeof(int); // 4096 of mem - sizeof(int)
-    char buffer[4096-sizeof(size_t)-sizeof(size_t)-512];      //extended by external code
->>>>>>> b83ff07a137c9c8f5f7bf0677384c33cc0506be9
 };
 
 #endif
